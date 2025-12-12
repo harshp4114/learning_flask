cdb71eb901b01c19627aaa22b29d9b849ab7b52e
harsh patadia <harshpatadia4114@gmail.com>
2025-12-12 17:04:20 +0530

    added the followers functionality using many to many relation, added post form, added follow unfollow button, displaying general posts in explore page

--- app/__init__.py
+++ app/__init__.py
@@ -30,9 +30,6 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-if not app.debug:
-    # ...
-
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
