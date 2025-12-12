b5d779a19c9e9340dea0eccc9dc27e457586384f
harsh patadia <harshpatadia4114@gmail.com>
2025-12-12 18:49:36 +0530

    added pagination

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config(object):
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
     ADMINS=['harshpatadia4114@gmail.com']
+    POSTS_PER_PAGE=3
